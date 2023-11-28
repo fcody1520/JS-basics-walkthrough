eb6f1e6b408553a183c7bcc0db56ff4a3cb347d3
Cody Farrington <fcody1520@gmail.com>
2023-11-28 11:29:41 -0700

    initial commit: strings file done

---
+++ strings.js
@@ -0,0 +1,14 @@
+const hello ="Hello, world!"
+let stringWithSingleQuotes = 'Single qoutes work too!'
+
+console.log(hello[0])
+
+const ball = 'ball'
+const templateString = `foot${ball}`
+
+console.log(templateString)
+
+let yourName = 'steve'
+console.log(`hi ${yourName}`)
+
+console.log('hi, '  + yourName)
