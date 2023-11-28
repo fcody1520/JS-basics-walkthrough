c16809240ea9fc70f5e2b97c12b715643a66f94b
Cody Farrington <fcody1520@gmail.com>
2023-11-28 12:41:45 -0700

    adds numbers and calc files

---
+++ numbers.js
@@ -0,0 +1,7 @@
+let a = 8
+
+// a = a + 12
+// is the same as 
+// a++ is a adding one each time
+a+=12 
+console.log(a)
--- strings.js
+++ strings.js
@@ -11,4 +11,4 @@ console.log(templateString)
 let yourName = 'steve'
 console.log(`hi ${yourName}`)
 
-console.log('hi, '  + yourName)
+console.log('hi, '  + yourName + "!")
